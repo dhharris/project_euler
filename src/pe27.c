dba845b35eeab83e01d52fdcb14ba7c4d5955036
David Harris <dhharris9@gmail.com>
2016-06-07 11:38:59 -0500

    formatting

--- src/pe27.c
+++ src/pe27.c
@@ -1,4 +1,5 @@
-/* Find the product of the coefficients, a and b, for the quadratic expression
+/*
+ * Find the product of the coefficients, a and b, for the quadratic expression
  * that produces the maximum number of primes for consecutive values of n,
  * starting with n = 0.
  */
