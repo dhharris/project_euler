7c9b7401c50785fdbfb1bcc30dcc3d4b01edd272
David Harris <dhharris9@gmail.com>
2016-06-07 09:40:06 -0500

    problem 27

---
+++ src/pe27.c
@@ -0,0 +1,49 @@
+/* Find the product of the coefficients, a and b, for the quadratic expression
+ * that produces the maximum number of primes for consecutive values of n,
+ * starting with n = 0.
+ */
+#include <stdio.h>
+
+int is_prime(int n)
+{
+    int i;
+
+    /* Base case */
+    if (n == 2)
+        return 1;
+    if (n < 0 || n % 2 == 0)
+        return 0;
+
+    for (i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return 0;
+
+    return 1;
+}
+
+int nprimes(int a, int b)
+{
+    int n;
+
+    for (n = 0; is_prime(n * n + a * n + b); ++n);
+    return n;
+}
+
+int main()
+{
+    int max = 0;
+    int prod = 0;
+    int a, b, n;
+
+    /* a must be odd, b must be odd and positive */
+    for (a = -999; a < 1000; a += 2)
+        for (b = 1; b < 1000; b += 2)
+            if ((n = nprimes(a, b)) > max) {
+                max = n;
+                prod = a * b;
+            }
+
+    printf("%d\n", prod);
+    
+    return 0;
+}
