1c63ebf30b284fb686400ba8b3cb09a97d30d63b
David Harris <dhharris9@gmail.com>
2016-06-08 09:56:34 -0500

    changed function name for readability

--- src/pe36.py
+++ src/pe36.py
@@ -1,12 +1,12 @@
 # Find the sum of all numbers less than one million which are palindromic
 # in base 10 and base 2
 
-def is_palindrome(s):
+def is_pal(s):
     return s == s[::-1]
 
 count = 0
 for i in range(1, 1000000, 2):
-    if is_palindrome(str(i)) and is_palindrome(str(bin(i))[2:]):
+    if is_pal(str(i)) and is_pal(str(bin(i))[2:]):
             count += i
 
 print count
