69f7a0ac719d430bd2fed8d770373610b24d04c4
David Harris <dhharris9@gmail.com>
2016-06-28 10:48:05 -0500

    problem 63

---
+++ src/pe63.py
@@ -0,0 +1,24 @@
+# How many n-digit positive integers exist which are also an nth power?
+
+def count_digits(n):
+    count = 0
+    while (n):
+        count += 1
+        n /= 10
+    return count
+
+def check(base, exponent):
+    '''We are checking to see if i^n has n digits'''
+    power = base**exponent
+    if count_digits(power) == exponent:
+        return 1
+    return 0
+
+
+count = 0
+for i in range(1, 25):
+    for n in range(1, 25):
+        if check(i, n):
+            count += 1
+
+print count
